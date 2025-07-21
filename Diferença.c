b47882060afbadc2a0bdc617abf5add2eaebda51
gabicolussi <gabriela.colussi.10@gmail.com>
2025-07-21 13:44:31 -0300

    Primeiro Commit

---
+++ Diferença.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main()
+{
+   int a[6], b[6], c[12];
+   int i, cont=0,j,achou ;
+   
+   printf("Digite os 6 elementos do vetor:\n");
+   for (i = 0; i < 6; i++) {
+       scanf("%d", &a[i]);
+   }
+   
+   printf("Digite os 6 elementos do vetor:\n");
+   for (i = 0; i < 6; i++) {
+       scanf("%d", &b[i]);
+   }
+
+
+    for (i = 0; i < 6; i++) {
+       printf("%d", a[i]);
+       printf("%d", b[i]);
+   }
+    
+    printf("\n");
+    
+ 
+
+    for (i = 0; i < 6; i++){
+       achou=0;
+        for (j = 0; j < 6; j++){
+            if(a[i]==b[j]){
+                achou=1;
+            }
+            
+            
+        }
+        
+        if(achou==0){
+            c[cont]=a[i]; 
+            cont++;
+        }
+}
+    
+    for (i = 0; i < cont; i++){
+        printf("%d\n", c[i]);
+    }
+    
+    
+return 0;
+}
+
---
+++ União.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+   int a[6], b[6], c[12];
+   int i, cont=0,j,achou ;
+   
+   printf("Digite os 6 elementos do vetor:\n");
+   for (i = 0; i < 6; i++) {
+       scanf("%d", &a[i]);
+   }
+   
+   printf("Digite os 6 elementos do vetor:\n");
+   for (i = 0; i < 6; i++) {
+       scanf("%d", &b[i]);
+   }
+
+
+    for (i = 0; i < 6; i++) {
+       printf("%d", a[i]);
+       printf("%d", b[i]);
+   }
+    
+    printf("\n");
+    
+
+    for (i = 0; i < 6; i++){
+        c[cont]=a[i];
+        cont++;
+        
+    }
+     for (i = 0; i < 6; i++){
+        c[cont]=b[i];
+        cont++;
+        
+    }
+    for (i = 0; i < cont; i++){
+        printf("%d\n", c[i]);
+    }
+    
+    
+return 0;
+}
